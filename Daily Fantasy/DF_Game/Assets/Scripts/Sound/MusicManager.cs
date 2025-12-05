224e732dcadf62e7419b7689888d28b743307785
Nert1166 <nikgorbunov2@gmail.com>
2025-12-05 18:30:38 +0300

    Added new player animations + UI fixes

--- Daily Fantasy/DF_Game/Assets/Scripts/Sound/MusicManager.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/Sound/MusicManager.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour
 {
     public static MusicManager Instance = null;
-    public AudioSource musicSource;
-
+    
     private void Awake()
     {
         if (Instance == null)
