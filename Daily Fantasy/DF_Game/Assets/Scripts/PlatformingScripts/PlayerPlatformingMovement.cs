a4ac1c42a10a079044716105606f562f2c7ad1ee
Lusiuk <brcloud1@yandex.ru>
2026-03-09 00:33:25 +0300

    Added Jumping and groundCheck

--- Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/PlayerPlatformingMovement.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/PlayerPlatformingMovement.cs
@@ -1,26 +1,61 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerPlatformingMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movement")]
     public float moveSpeed = 5f;
-
     float horizontalMovement;
+
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(horizontalMovement *  moveSpeed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocityY);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpPower);
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpPower * 0.5f);
+            }
+        }
+    }
+
+    private bool isGrounded() => Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer) ? true : false;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
 }
