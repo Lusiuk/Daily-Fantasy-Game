01895e6d4bfeca73c506f59107726fb80ba638fe
Lusiuk <brcloud1@yandex.ru>
2026-03-08 20:13:30 +0300

    Added a base for platforming

---
+++ Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/PlayerPlatformingMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerPlatformingMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    float horizontalMovement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontalMovement *  moveSpeed, rb.linearVelocityY);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}
---
+++ Daily Fantasy/DF_Game/Assets/Sprites/Platforming/Platformer Character Template 16x16/README.txt
@@ -0,0 +1,13 @@
+Pack By: Propdev
+
+--------------------------------------------------------
+
+When you buy this pack, you are allowed to:
+- Use it in any project, even commercially
+- Edit it so it fits your project
+But you are *not* allowed to:
+- Call this pack yours
+
+--------------------------------------------------------
+
+Thank you so much for getting my asset pack!
