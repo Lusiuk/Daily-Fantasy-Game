b352d93b946f7eef6e1af3ab8d6e0888cee4f4f6
Nert1166 <nikgorbunov2@gmail.com>
2026-03-21 20:38:43 +0300

    Added beat scroller

---
+++ Daily Fantasy/DF_Game/Assets/Scripts/Rhythm Game/BeatScroller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+    public float beatTempo; // The tempo of the beats in beats per minute (BPM)
+
+    public bool hasStarted; // Flag to indicate if the beat scroller has started
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        beatTempo = beatTempo / 60f; // Convert BPM to beats per second
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+    }
+}
