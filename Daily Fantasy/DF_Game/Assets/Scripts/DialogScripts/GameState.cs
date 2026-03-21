75679d01a9c33cea581a44e5853672ea1d783c95
YariloDia <Xan-Yar0slav@yandex.ru>
2026-03-21 23:37:36 +0300

    Preparing for saves

--- Daily Fantasy/DF_Game/Assets/Scripts/DialogScripts/GameState.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/DialogScripts/GameState.cs
@@ -35,4 +35,75 @@ public static class GameState
 
         Debug.Log("GameState: Состояние игры сброшено");
     }
+
+    // Ключ для сохранения
+    private const string SAVE_KEY = "GameState";
+
+    // Сохранить состояние
+    public static void Save()
+    {
+        SaveData data = new SaveData
+        {
+            previousScene = PreviousScene,
+            initialScene = InitialScene,
+            isFirstLoad = IsFirstLoad,
+            isMinigameCompleted = IsMinigameCompleted,
+            minigameName = MinigameName,
+            isDoor1Completed = IsDoor1Completed,
+            isDoor2Completed = IsDoor2Completed,
+            isDoor3Completed = IsDoor3Completed
+        };
+
+        string json = JsonUtility.ToJson(data);
+        PlayerPrefs.SetString(SAVE_KEY, json);
+        PlayerPrefs.Save();
+        Debug.Log("GameState: Сохранено");
+    }
+
+    // Загрузить состояние
+    public static void Load()
+    {
+        if (PlayerPrefs.HasKey(SAVE_KEY))
+        {
+            string json = PlayerPrefs.GetString(SAVE_KEY);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            PreviousScene = data.previousScene;
+            InitialScene = data.initialScene;
+            IsFirstLoad = data.isFirstLoad;
+            IsMinigameCompleted = data.isMinigameCompleted;
+            MinigameName = data.minigameName;
+            IsDoor1Completed = data.isDoor1Completed;
+            IsDoor2Completed = data.isDoor2Completed;
+            IsDoor3Completed = data.isDoor3Completed;
+
+            Debug.Log("GameState: Загружено");
+        }
+        else
+        {
+            Debug.Log("GameState: Нет сохранений, используем значения по умолчанию");
+        }
+    }
+
+    // Сброс сохранения
+    public static void DeleteSave()
+    {
+        PlayerPrefs.DeleteKey(SAVE_KEY);
+        PlayerPrefs.Save();
+        Debug.Log("GameState: Сохранение удалено");
+    }
+
+    // Структура данных для сохранения
+    [System.Serializable]
+    private class SaveData
+    {
+        public string previousScene;
+        public string initialScene;
+        public bool isFirstLoad;
+        public bool isMinigameCompleted;
+        public string minigameName;
+        public bool isDoor1Completed;
+        public bool isDoor2Completed;
+        public bool isDoor3Completed;
+    }
 }
