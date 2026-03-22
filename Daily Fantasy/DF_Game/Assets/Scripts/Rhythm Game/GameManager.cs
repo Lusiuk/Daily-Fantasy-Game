07efe84f76385e0f1af6342c2bbe69341e1ce933
Nert1166 <nikgorbunov2@gmail.com>
2026-03-22 14:45:05 +0300

    Added score/multiplier mechanics + idle fix

--- Daily Fantasy/DF_Game/Assets/Scripts/Rhythm Game/GameManager.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/Rhythm Game/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,11 +7,21 @@ public class GameManager : MonoBehaviour
     public bool startPlaying;
     public BeatScroller theBS;
     public static GameManager instance;
+    public int currentScore;
+    public int scorePerNote = 100;
 
+    public Text scoreText;
+    public Text multiText;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThreshold;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
+        scoreText.text = "Очки: 0";
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -31,11 +42,30 @@ public class GameManager : MonoBehaviour
     public void NoteHits()
     {
         Debug.Log("Hit on time!");
+
+        if (currentMultiplier - 1 < multiplierThreshold.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThreshold[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Множитель: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Очки: " + currentScore;
     }
 
     public void NoteMiss()
     {
         Debug.Log("Missed note!");
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Множитель: x" + currentMultiplier;
     }
 
 }
