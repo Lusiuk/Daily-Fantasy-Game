d8e5ff060e2dfa7ee5cede4f18adbc77a0d065af
Lusiuk <brcloud1@yandex.ru>
2026-03-26 22:13:23 +0300

    Added platforming2 draft

--- Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/GameController.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/GameController.cs
@@ -100,6 +100,7 @@ public class GameController : MonoBehaviour
     public void ResetGame()
     {
         Debug.Log("ResetGame called");
+        player.GetComponent<Rigidbody2D>().linearVelocityY = 0f;
         Time.timeScale = 1;
         animator.SetBool("dead", false);
         animator.Play("Idle", -1, 0f);
