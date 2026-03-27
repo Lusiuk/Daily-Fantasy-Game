3a893f6806ccf6442baec0b44b0129a5e2aed57c
Lusiuk <brcloud1@yandex.ru>
2026-03-27 14:44:47 +0300

    Added finished to rhytm game

--- Daily Fantasy/DF_Game/Assets/Scripts/Rhythm Game/GameManager.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/Rhythm Game/GameManager.cs
@@ -1,12 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("General Settings")]
     public AudioSource theMusic;
     public bool startPlaying;
     public BeatScroller theBS;
     public static GameManager instance;
+
+    public Dialogue completionDialogue;
+
+    public GameObject resultButton;
+
+    [Header("Scores and Results")]
+
     public int currentScore;
     public int scorePerNote = 100;
     public int scorePerGoodNote = 125;
@@ -21,15 +31,23 @@ public class GameManager : MonoBehaviour
     public float PerfectHitsCount;
     public float MissedHitsCount;
 
+    [Header("Finishing info")]
+
     public GameObject resultsScreen;
     public Text percentHitText, rankText, normalText, goodText, perfectText, missedText, finalScoreText;
 
+    [Header("Multipliers")]
     public int currentMultiplier;
     public int multiplierTracker;
     public int[] multiplierThreshold;
+
+    private bool resultsShown = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        resultButton.SetActive(false);
+        resultsScreen.SetActive(false);
         instance = this;
         scoreText.text = "Очки: 0";
         currentMultiplier = 1;
@@ -52,46 +70,79 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            if (!theMusic.isPlaying && !resultsScreen.activeInHierarchy && !resultsShown)
             {
-                resultsScreen.SetActive(true);
-
-                normalText.text = "" + NormalHitsCount;
-                goodText.text = "" + GoodHitsCount;
-                perfectText.text = "" + PerfectHitsCount;
-                missedText.text = "" + MissedHitsCount;
-
-                float percentHit = Mathf.Round(((NormalHitsCount + GoodHitsCount + PerfectHitsCount) / totalNotes) * 100f);
-                percentHitText.text = "" + percentHit + "%";
-
-                string rankValue = "F";
-                if(percentHit > 40)
-                {
-                    rankValue = "D";
-                    if(percentHit > 60)
-                    {
-                        rankValue = "C";
-                        if(percentHit > 75)
-                        { 
-                            rankValue = "B";
-                            if(percentHit > 85)
-                            {
-                                rankValue = "A";
-                                if (percentHit > 95)
-                                {
-                                    rankValue = "S";
-                                }
-                            }
-                        }
-                    }
-                }
-
-                rankText.text = rankValue;
-
-                finalScoreText.text = currentScore.ToString();
+                resultsShown = true;
+                StartCoroutine(ShowResultsAfterDelay());
+                return;
             }
         }
     }
+
+    private IEnumerator ShowResultsAfterDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        resultsScreen.SetActive(true);
+        normalText.text = "" + NormalHitsCount;
+        goodText.text = "" + GoodHitsCount;
+        perfectText.text = "" + PerfectHitsCount;
+        missedText.text = "" + MissedHitsCount;
+
+        float percentHit = Mathf.Round(((NormalHitsCount + GoodHitsCount + PerfectHitsCount) / totalNotes) * 100f);
+        percentHitText.text = "" + percentHit + "%";
+
+        string rankValue = "F";
+        if (percentHit > 40)
+        {
+            rankValue = "D";
+            if (percentHit > 60)
+            {
+                rankValue = "C";
+                if (percentHit > 75)
+                {
+                    rankValue = "B";
+                    if (percentHit > 85)
+                    {
+                        rankValue = "A";
+                        if (percentHit > 95)
+                        {
+                            rankValue = "S";
+                        }
+                    }
+                }
+            }
+        }
+
+        rankText.text = rankValue;
+        finalScoreText.text = currentScore.ToString();
+        resultButton.SetActive(true);
+    }
+
+    public void OnResultPressed()
+    {
+        if (resultsScreen != null)
+            resultsScreen.SetActive(false);
+        if (resultButton != null)
+            resultButton.SetActive(false);
+
+        // Устанавливаем флаг завершения мини-игры
+        GameState.IsRhythmGame1Completed = true;
+        GameState.Save();
+        Debug.Log("Мини-игра отмечена как завершённая в GameState");
+
+        // Показ диалога
+        if (completionDialogue != null && DialogueSystem.Instance != null)
+        {
+            Debug.Log($"Calling completion dialogue: {completionDialogue.name}, timeScale={Time.timeScale}");
+            DialogueSystem.Instance.ShowDialogue(completionDialogue);
+        }
+        else
+        {
+            Debug.LogWarning($"Completion dialogue NOT shown. completionDialogue={(completionDialogue ? completionDialogue.name : "null")}, DialogueSystem.Instance={(DialogueSystem.Instance ? "ok" : "null")}");
+        }
+    }
+
+
 
     public void NoteHits()
     {
