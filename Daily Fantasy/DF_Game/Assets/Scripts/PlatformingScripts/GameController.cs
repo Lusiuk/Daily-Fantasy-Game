6d30f04c083313753c5862e29791ac1276fb7ea5
Lusiuk <brcloud1@yandex.ru>
2026-05-06 12:08:18 +0300

    Fixed time issue and confusion with wall_jumping

--- Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/GameController.cs
+++ Daily Fantasy/DF_Game/Assets/Scripts/PlatformingScripts/GameController.cs
@@ -95,7 +95,7 @@ public class GameController : MonoBehaviour
     private IEnumerator ShowCompletionDialogueAfterDelay()
     {
         Time.timeScale = 1f;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0.01f);
 
 
         // Устанавливаем флаг завершения мини-игры
